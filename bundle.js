bb911ee57406d4feb62c72f52c182e388e9e7db9
Pablo Ignacio Ochoa <58561140+PabloIgnacio00@users.noreply.github.com>
2023-07-13 19:09:19 -0600

    Create bundle.js

---
+++ bundle.js
@@ -0,0 +1,10 @@
+import React, ReactDOM from 'react'
+
+const mountNode = document.getElementById('widget-mount-point');
+
+// Extract the token value from the URL parameters
+const urlParams = new URLSearchParams(window.location.search);
+const token = urlParams.get('token');
+
+// Render your React component into the DOM element, passing the token as a prop
+ReactDOM.render(<Chatbot token={token} />, mountNode);
