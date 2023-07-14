32ba01c218234b16023a76742bc7e4cf7fbcf320
Pablo Ignacio Ochoa <58561140+PabloIgnacio00@users.noreply.github.com>
2023-07-13 19:08:39 -0600

    Create chatbot.js

---
+++ chatbot.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Chatbot = ({ token }) => {
+  // You can use the token value within your component to customize its behavior
+  // For example, you can make API calls or display different content based on the token
+
+  // Example usage: Displaying the token value
+  return (
+    <div>
+      <h1>My Component</h1>
+      <p>Token: {token}</p>
+      <p>This is an example React component.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
